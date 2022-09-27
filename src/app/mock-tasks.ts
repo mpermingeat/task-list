55f5246aca3c4a070c4868f0b9eb8191f9d2281a
MaxP <maxpermingeat@gmail.com>
2022-09-27 10:22:56 -0300

    lista de task

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+
+import { Task } from "./task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "terminar primer modulo",
+        day: "agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "terminar segundo modulo",
+        day: "agosto 12 las 12:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "terminar tercer modulo",
+        day: "septiembre 5 a las 12:00",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "leer libro",
+        day: "septiembre 15 a las 12:00",
+        reminder: false
+    }
+]
