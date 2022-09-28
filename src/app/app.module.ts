f06104e15e6b167eb8a01cdcfd0a0e34179e92a1
MaxP <maxpermingeat@gmail.com>
2022-09-28 19:20:51 -0300

    Task List terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule  } from '@angular/forms';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -11,6 +13,9 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+
+
+
 
 
 @NgModule({
@@ -27,7 +32,8 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule 
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
